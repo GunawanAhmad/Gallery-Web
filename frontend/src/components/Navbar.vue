7ed85eef12c915ae827468c70d87d6b32afdbff4
Gunawan Nur Ahmad <54525132+Gunawanart@users.noreply.github.com>
2020-11-15 22:02:40 +0700

    add a tag to list

--- frontend/src/components/Navbar.vue
+++ frontend/src/components/Navbar.vue
@@ -8,28 +8,33 @@
       </div>
       <ul class="nav__ul">
           <li class="nav__list search" >
-              <input type="text" ref="searchInput" @focus="showResultBar" @focusout="showResultBar" v-model="inputSearch">
+              <input type="text" ref="searchInput"  @focusout="closeResultBar" @focus="openResultBar" v-model="inputSearch" >
               <span class="search__logo" @click="showSearchInput" ref="searchLogo">
                   <i class="fas fa-search"></i>
               </span>
               <div class="search-result" v-show="resultShow"> 
                   <ul ref="suggestionBar" @mouseover="suggestBoxHover" @mouseout="suggestBoxHover">
+                      <li @click="selectSuggestion(inputSearch)">{{ inputSearch }}</li>
                       <li v-for="(data,index) in arrSearchResult" :key="index" @click="selectSuggestion(data)">{{ data }}</li>
                   </ul>
               </div>
               
               
           </li>
-          <li class="nav__list name">Gunawan</li>
+          <li class="nav__list name">{{ username }}</li>
           <li class="img-profile nav__list">
-              <div class="img" @click="showMenu"></div>
+              <div class="img" @click="toggleMenu"></div>
               <ul class="menu__list" ref="menuList">
-                  <li>Your profile</li>
-                  <li>Setting</li>
-                  <li>Log out</li>
+                  <li @click="toggleMenu">New album</li>
+                  <li @click="toggleMenu">
+                      <router-link :to="'/' + username">Your profile</router-link>
+                  </li>
+                  <li @click="toggleMenu">Setting</li>
+                  <li @click="toggleMenu">Log out</li>
               </ul>
           </li>
       </ul>
+      <div class="invis-background" ref="invisBack" @click="toggleMenu"></div>
      
   </nav>
 </template>
@@ -39,36 +44,58 @@ export default {
     data() {
         return {
             resultShow : false,
-            arrSearchResult : ['makan', 'ayam'],
-            inputSearch : ''
+            arrSearchResult : ['makan'],
+            inputSearch : '',
+            username : 'Gunawan'
+        }
+    },
+    watch : {
+        inputSearch() {
+            if(this.inputSearch != '') {
+                this.resultShow = true;
+                this.$refs.searchInput.classList.add('result-show')
+
+                //search from the backend logic
+            } else {
+                this.resultShow = false
+                this.$refs.searchInput.classList.remove('result-show')
+            }
         }
     },
     methods : {
-        showMenu() {
+        toggleMenu() {
             this.$refs.menuList.classList.toggle('show')
+            this.$refs.invisBack.classList.toggle('show')
         },
         showSearchInput() {
             this.$refs.searchInput.classList.toggle('show')
             this.$refs.searchLogo.classList.toggle('show')
-            this.inputSearch = ''
+            this.inputSearch = '';
         },
         showSearchResult() {
-            
             console.log('masuk')
         },
-        showResultBar() {
+
+        //close suggestion bar when input is out of focus
+        closeResultBar() {
             if(!this.$refs.suggestionBar.classList.contains('hovered')) {
-                this.resultShow = !this.resultShow;
+                this.resultShow = false;
+                this.$refs.searchInput.classList.remove('result-show')
             }
-    
-            if (this.arrSearchResult.length > 0) {
-                this.$refs.searchInput.classList.toggle('result-show')
-            }
+            
             
         },
         suggestBoxHover() {
             this.$refs.suggestionBar.classList.toggle('hovered')
         },
+
+        //open suggestion bar when input is in focus
+        openResultBar() {
+            if(this.inputSearch != '') {
+                this.resultShow = true;
+                this.$refs.searchInput.classList.add('result-show')
+            }
+        },  
         selectSuggestion(data) {
             this.inputSearch = data;
             this.resultShow = false;
